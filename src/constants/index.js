96039705fecd8012b4743be29c8cf489fe44c703
stealth1710 <adnan.ab164@gmail.com>
2024-12-23 21:09:32 +0000

    hadth add

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ import project2 from "../assets/projects/supermarkets.jpeg";
 import project3 from "../assets/projects/project-3.jpg";;
 import fashionMnist from "../assets/projects/fashionmnist.jpeg";
 import rota from "../assets/projects/rota.jpg";
-
+import hadith from "../assets/projects/hadiths.jpg"
 export const HERO_CONTENT = `I am a skilled developer with expertise in Python programming and Full Stack Development, specializing in creating responsive, user-centric applications and scalable backend systems. With a focus on delivering innovative solutions, I am dedicated to continuous learning and staying at the forefront of emerging technologies.`;
 
 export const ABOUT_TEXT = ` A BSc Computer Science graduate from the University of Roehampton with a strong foundation in programming, specializing in Python and Full Stack Development. I have experience building dynamic and responsive web applications, combining intuitive frontend design with secure and scalable backend systems. Proficient in tools and technologies like Python libraries, JavaScript, React, Node.js, and databases (MySQL, MongoDB), I am passionate about solving complex problems and delivering high-quality, innovative solutions. With a commitment to continuous learning, I aim to contribute to impactful projects and make a difference in the tech industry.`;
@@ -11,6 +11,13 @@ export const ABOUT_TEXT = ` A BSc Computer Science graduate from the University 
 
 
 export const PROJECTS = [
+  {
+    title: "Hadith Generator  ",
+    image: hadith ,
+    description:
+      " A MERN stack website generating random hadiths ",
+    technologies: ["Mongo DB","Express JS","React JS","Node JS"],
+  },
   {
     title: "Accomodation booking App",
     image: project1,
@@ -46,7 +53,8 @@ export const PROJECTS = [
     description:
       "A machine learning model built using python to classify fashion Mnist images ",
     technologies: ["Python"],
-  }
+  },
+
 ];
 
 export const CONTACT = {
